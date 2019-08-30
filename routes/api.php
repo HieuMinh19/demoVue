029c637ba72e47eca7efe43dad98786bd5daafce
HieuMinh19 <lehieuminh231@gmail.com>
2019-08-30 17:30:41 +0700

    pagination

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +10,16 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// List articles
+Route::get('articles', 'ArticleController@index');
+// List single article
+Route::get('article/{id}', 'ArticleController@show');
+// Create new article
+Route::post('article', 'ArticleController@store');
+// Update article
+Route::put('article', 'ArticleController@store');
+// Delete article
+Route::delete('article/{id}', 'ArticleController@destroy');
